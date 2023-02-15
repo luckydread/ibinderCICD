89dae0edabdbf0e0975206af5662d200179b536d
john chimbani <john.chimbani@outlook.com>
2023-02-15 15:14:09 +0100

    adding terraform steps to workflow.

--- terraform/key_vault/main.tf
+++ terraform/key_vault/main.tf
@@ -2,11 +2,11 @@
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "issuereporting" {
-  name                        = var.azurerm_key_vault_name
-  location                    = var.resource_group_location
-  resource_group_name         = var.resource_group_name
- # enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  name                = var.azurerm_key_vault_name
+  location            = var.resource_group_location
+  resource_group_name = var.resource_group_name
+  # enabled_for_disk_encryption = true
+  tenant_id = data.azurerm_client_config.current.tenant_id
   #soft_delete_retention_days  = 7
   #purge_protection_enabled    = false
   sku_name = "standard"
@@ -17,7 +17,7 @@ resource "azurerm_key_vault" "issuereporting" {
       "Get",
     ]
     secret_permissions = [
-      "Get","List", "Set"
+      "Get", "List", "Set"
     ]
     storage_permissions = [
       "Get",
