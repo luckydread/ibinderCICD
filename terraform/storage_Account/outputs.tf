d6acc5f43705b719a70ad124b3b4ff87baaf2037
john chimbani <john.chimbani@outlook.com>
2023-02-16 14:05:09 +0100

    adding app service.

--- terraform/storage_Account/outputs.tf
+++ terraform/storage_Account/outputs.tf
@@ -1,4 +1,4 @@
 output "connection_string" {
-  description = "Storage account coonection string"
+  description = "Storage account conection string"
   value       = azurerm_storage_account.issuereporting.primary_connection_string
 }
