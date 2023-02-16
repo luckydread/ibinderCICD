ff7b3d380b4bd12ff6b6a44ba004c27fb8644fec
john chimbani <john.chimbani@outlook.com>
2023-02-16 08:58:59 +0100

    enable purge prtection.

--- terraform/key_vault/main.tf
+++ terraform/key_vault/main.tf
@@ -5,10 +5,9 @@ resource "azurerm_key_vault" "issuereporting" {
   name                = var.azurerm_key_vault_name
   location            = var.resource_group_location
   resource_group_name = var.resource_group_name
-  # enabled_for_disk_encryption = true
+  enabled_for_disk_encryption = true
   tenant_id = data.azurerm_client_config.current.tenant_id
-  #soft_delete_retention_days  = 7
-  #purge_protection_enabled    = false
+  purge_protection_enabled    = true
   sku_name = "standard"
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
