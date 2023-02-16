85a18bb2e17b05af19d4b1be39ec195963f52d9c
john chimbani <john.chimbani@outlook.com>
2023-02-16 13:36:25 +0100

    adding app service.

--- terraform/storage_Account/variables.tf
+++ terraform/storage_Account/variables.tf
@@ -7,11 +7,7 @@ variable "resource_group_location" {
   description = "The resource group location."
   type        = string
 }
-variable "azurerm_application_insights_name" {
-  description = "The application insights name."
-  type        = string
 
-}
 
 variable "azurerm_storage_account_name" {
   description = "The storage account name."
