892408429437bff520d48beaff6873574e370e85
munemo <122976266+luckydread@users.noreply.github.com>
2023-02-17 08:41:18 +0100

    deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,6 +43,9 @@ module "application_insights" {
   azurerm_application_insights_name = var.azurerm_application_insights_name
 }
 
+
+
+
 module "storage_account" {
   source                       = "./storage_Account"
   resource_group_name          = var.resource_group_name
